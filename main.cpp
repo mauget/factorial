613bb4386b184b80c3f276da85fb39aed77a9d12
mauget <mauget@gmail.com>
2019-10-02 08:04:23 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <unordered_map>
+
+std::unordered_map<long, long> cache;
+
+// Use a cache to lookup earlier factorial results
+
+long factorial(long n) {
+    if (n <= 1) return 1;
+
+    if (cache.count(n) > 0) return cache[n];
+    cache[n] = n * factorial(n - 1);
+    return cache[n];
+}
+
+
+int main() {
+    for (long a = 20; a >= 0; a--) {
+        long f = factorial(a);
+        std::cout << "factorial " << a << " is " << factorial(a) << std::endl;
+    }
+
+}
